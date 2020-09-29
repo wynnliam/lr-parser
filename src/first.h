1cb901389029fbc8174893735f299fe611e0d340
Liam Wynn <wynnliam@pdx.edu>
2020-09-29 13:47:31 -0700

    Added first.h

---
+++ src/first.h
@@ -0,0 +1,39 @@
+// Liam Wynn, 9/29/2020, LR Parser
+
+/*
+	Stores all procedures related to computing
+	the FIRST operation for a grammar symbol.
+
+	Let a be a string of grammar symbols,
+	then FIRST(a) is the set of terminals
+	that begin the strings derived from a.
+
+	Here's the rules for computing FIRST:
+
+	1. If X is a single terminal FIRST(X) = {X}.
+
+	2. If X -> e (empty) is a production, then
+	add e to FIRST(X).
+
+	3. If X is a nonterminal with a production
+	X -> Y_1 Y_2 ... Y_k, then put a in FIRST(X)
+	if for some i, a is in FIRST(Y_i) and e is in
+	all of FIRST(Y_1), ..., FIRST(Y_(i-1)).
+	If e is in FIRST(Y_j) for j = 1 ... k, add e
+	to FIRST(X).
+
+	Using these rules, you can compute the FIRST
+	for a string of symbols Q = X_1 X_2 ... X_n as follows:
+	Add all non-e symbols of FIRST(X_1) to FIRST(Q). Add
+	all non-e symbols of FIRST(X_k) if e is in FIRST(X_1), ..., FIRST(X_(k-1)).
+	Add e to FIRST(Q) if e is in all FIRST(X_i) (for i in 1 .. n).
+*/
+
+#ifndef FIRST
+#define FIRST
+
+#include "./grammar.h"
+
+
+
+#endif
