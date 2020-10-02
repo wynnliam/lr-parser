ea46d394a1d5573a93abda17027c332dbe3daeb9
Liam Wynn <wynnliam@pdx.edu>
2020-10-02 10:37:16 -0700

    Added globals.h

---
+++ src/globals.h
@@ -0,0 +1,12 @@
+// Liam Wynn, 10/2/2020, LR Parser
+
+/*
+	Keeps track of all global variables the system
+	will use.
+*/
+
+#include "./grammar.h"
+
+#define MAX_PRODUCTIONS	10
+
+struct production productions[MAX_PRODUCTIONS];
